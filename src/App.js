34a884b05d0c8135149832779006cbe639f0ed1f
Milin Mestry <milinmestry@gmail.com>
2017-04-21 23:53:54 +0530

    setup React using npm and created first Hello World program.

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function formatName(User) {
+  return User.firstName + ' ' + User.lastName;
+}
+
+const user = {
+  firstName: 'Milin',
+  lastName: 'Mestry'
+};
+
+const elementWelcomeUser = (
+  <h1>Hello {formatName(user)}!</h1>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          {elementWelcomeUser}
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+
+export default App;
