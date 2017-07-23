f4b52afc85b7fe6b079e0417e77422cf19914072
Milin Mestry <milinmestry@gmail.com>
2017-07-23 20:19:03 +0530

    Fixed issues #1. related to having textbox and textarea in a single class NameForm; #2. Added a new dedicated class file to display all FORM elements inside form DIV.

--- src/Form.js
+++ src/Form.js
@@ -12,6 +12,12 @@ class Form extends Component {
   render() {
     return (
       <div className="App">
+        <h3>Forms</h3>
+        <p>
+          Overall, this makes it so that &lt;input type="text"&gt;, &lt;textarea&gt;, and &lt;select&gt;
+          all work very similarly - they all accept a <strong>value attribute</strong> that
+          you can use to implement a controlled component.
+        </p>
         <div id="textbox">{elementTextbox}</div>
         <div id="select">{elementSelect}</div>
       </div>
